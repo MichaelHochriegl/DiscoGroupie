46c127d692c702d6c50bd1df3d3ebfbccca66dae
Dr_Cox1911 <michael.hochriegl@viking-labs.de>
2022-01-03 20:14:08 +0100

    Add basic Discord bot setup

---
+++ src/Infrastructure/Discord/SetupHost.cs
@@ -0,0 +1,46 @@
+using DiscoGroupie.Infrastructure.Discord.Services;
+using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DiscoGroupie.Infrastructure.Discord;
+
+public static class SetupHost
+{
+    public static IHostBuilder SetupDiscord(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureDiscordHost((context, config) =>
+        {
+            config.SocketConfig = new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+                AlwaysDownloadUsers = true,
+                MessageCacheSize = 200
+            };
+
+            config.Token = context.Configuration.GetValue<string>("DiscordBotSettings:Token");
+        });
+
+        hostBuilder.UseCommandService((context, config) =>
+        {
+            config.CaseSensitiveCommands = false;
+            config.LogLevel = LogSeverity.Verbose;
+            config.DefaultRunMode = RunMode.Async;
+        });
+
+        hostBuilder.ConfigureServices((context, services) =>
+        {
+            services.AddHostedService<CommandHandlerService>();
+            services.AddHostedService<BotStatusService>();
+                
+        });
+
+        hostBuilder.UseConsoleLifetime();
+            
+        return hostBuilder;
+    }
+}
--- src/UI/Bot/Program.cs
+++ src/UI/Bot/Program.cs
@@ -1,7 +1,44 @@
+using DiscoGroupie.Infrastructure.Discord;
 using DiscoGroupie.UI.Bot;
+using Serilog;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.Development.json")
+    .AddEnvironmentVariables()
     .Build();
 
-await host.RunAsync();
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+try
+{
+    Log.Information("PzDiscoB starting up...");
+    var host = Host.CreateDefaultBuilder(args)
+        .UseSerilog()
+        .ConfigureServices(services =>
+        {
+            // services.AddHostedService<Worker>();
+            // services.AddPersistence(configuration);
+        })
+        .SetupDiscord()
+        .Build();
+    
+    // using (var scope = host.Services.CreateScope())
+    // {
+    //     var db = scope.ServiceProvider.GetRequiredService<DiscoGroupieNpsqlDbContext>();
+    //     db.Database.Migrate();
+    // }
+
+    await host.RunAsync();
+}
+catch (Exception exception)
+{
+    Log.Fatal(exception, "PzDiscoB failed to start!");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
